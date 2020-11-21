8e50c02fbcf6e2a0827fb316f658261d4e617448
Shlok Kamble <30517216+shlokk9763@users.noreply.github.com>
2020-11-21 23:17:27 +0530

    Added Setting Window  and File Getter

--- src/render.js
+++ src/render.js
@@ -3,4 +3,4 @@ let fileListBox = document.getElementById('fileListBox');
 
 // settingBtn.addEventListener('click', (e) => console.log(window.api.getCompleteFileListForPath("msg")))
 settingBtn.addEventListener('click', (e) => window.api.send("open-settings", "some data"))
-console.log(window.api.getCompleteFileListForPath("E:\\Work\\electron-work\\polymer-command-tool\\src")); 
+console.log(window.api.getCompleteFileListForPath("E:\\Work\\electron-work\\polymer-command-tool\\src"));
